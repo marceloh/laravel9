eb88feaa433ddbc9bb1eee86a1441be9a5844516
marcelo <marcelo@contassconsultoria.com.br>
2022-10-09 18:23:43 -0300

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Olá Mundo!, {{name}}</h2>
+    <br>
+    <input type="text" v-model="name">
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: 'Teste'
+            }
+        }
+    }
+</script>
